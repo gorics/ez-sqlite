ea796f40ef39137188c3eea5fb15abe824c2e751
박은성 <nydst9492@nydasan.hs.kr>
2023-01-31 02:34:45 +0900

    Create ezsqlite.py
    아직 많이 부족해서 쓰지마요 걍

---
+++ ezsqlite.py
@@ -0,0 +1,26 @@
+import sqlite3
+import time
+
+def sqlite(db, sql_query, *params):
+    while True:
+        try:
+            conn = sqlite3.connect(db)
+            c = conn.cursor()
+            c.execute(sql_query, params)
+            result = c.fetchall()
+            conn.commit()
+            break
+        except sqlite3.OperationalError as e:
+            if 'locked' in str(e):
+                time.sleep(0.1)
+                continue
+            else:
+                raise
+        finally:
+            conn.close()
+    return result
+
+# Example usage
+result = sqlite("example.db", "SELECT * FROM users WHERE username=?", ("user1",))
+for row in result:
+    print(row)
